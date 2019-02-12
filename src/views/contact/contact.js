18ef928aa172cf82f731a108ae2779c0a61402b1
asmarenas <asmarenas@gmail.com>
2019-02-12 09:10:16 +0100

    Change mobile navigation-menu animation

--- src/views/contact/contact.js
+++ src/views/contact/contact.js
@@ -6,9 +6,9 @@ import Hexagon from "../../components/hexagon/hexagon";
 class Contact extends React.Component {
   render() {
     return (
-      <section className="contact">
-        <div className="contact__form-grid">
-          <div className="contact__form-container">
+      <section className="contact-l-grid">
+        <div className="contact-l-grid__column--1">
+          <div className="contact-c-form">
             <h1>Contact</h1>
             <p>
               sit amet consectetur adipisicing elit. Facere, consectetur beatae
@@ -16,34 +16,34 @@ class Contact extends React.Component {
             <form className="">
               <input
                 type="text"
-                className="contact__input"
+                className="contact-c-form__input"
                 id="name"
                 placeholder="Your Name"
               />
               <input
                 type="text"
-                className="contact__input"
+                className="contact-c-form__input"
                 id="name"
                 placeholder="Your E-Mail"
               />
               <label htmlFor="message">Message</label>
               <textarea
-                className="contact__textarea"
+                className="contact-c-form__textarea"
                 name="message"
                 id="message"
               />
               <input
                 type="submit"
                 value="Send Message"
-                className="contact__btn"
+                className="contact-c-form__btn"
               />
             </form>
           </div>
         </div>
 
-        <div className="contact__shapes-grid">
-          <Hexagon className={"contact__hexagon-1"} />
-          <Hexagon className={"contact__hexagon-2"} />
+        <div className="contact-l-grid__column--2">
+          <Hexagon className={"contact-c-hexagon--1"} />
+          <Hexagon className={"contact-c-hexagon--2"} />
         </div>
       </section>
     );
