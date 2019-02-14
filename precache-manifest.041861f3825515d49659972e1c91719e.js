957f33b8491ad2919482661659febb4829547950
asmarenas <asmarenas@gmail.com>
2019-02-14 09:26:19 +0100

    Updates

---
+++ precache-manifest.041861f3825515d49659972e1c91719e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "70b2678749ecf0185edfbce104497de8",
+    "url": "/Geodian/static/media/home-background.70b26787.jpg"
+  },
+  {
+    "revision": "ef6c312373fbfe227a80",
+    "url": "/Geodian/static/css/main.c8870753.chunk.css"
+  },
+  {
+    "revision": "c9378c0a361a72e319fabbe4229b61ee",
+    "url": "/Geodian/static/media/bridge.c9378c0a.jpg"
+  },
+  {
+    "revision": "2c04c65c3e15bd764068",
+    "url": "/Geodian/static/js/1.2c04c65c.chunk.js"
+  },
+  {
+    "revision": "405f9dd3a4129245d459",
+    "url": "/Geodian/static/js/runtime~main.405f9dd3.js"
+  },
+  {
+    "revision": "1ae95fd9b04ebd6ca63e0e58e19fbe59",
+    "url": "/Geodian/static/media/aside.1ae95fd9.jpg"
+  },
+  {
+    "revision": "32539b6126989c8d9e7d3b54dd600628",
+    "url": "/Geodian/static/media/main-top.32539b61.jpg"
+  },
+  {
+    "revision": "ef6c312373fbfe227a80",
+    "url": "/Geodian/static/js/main.ef6c3123.chunk.js"
+  },
+  {
+    "revision": "aa1e4f604fb0bcc712f083acc93ee5b6",
+    "url": "/Geodian/static/media/golden-gate.aa1e4f60.jpg"
+  },
+  {
+    "revision": "6cfc6b3216509e6e317a86e1e8522105",
+    "url": "/Geodian/static/media/one-hundred-lights.6cfc6b32.jpg"
+  },
+  {
+    "revision": "caf66a15ee0c04d2250bb8dc90d03455",
+    "url": "/Geodian/static/media/secret-place.caf66a15.jpg"
+  },
+  {
+    "revision": "6cbf91160eb3c1c8299beb10fc3e2306",
+    "url": "/Geodian/static/media/building.6cbf9116.jpg"
+  },
+  {
+    "revision": "2c04c65c3e15bd764068",
+    "url": "/Geodian/static/css/1.e36c7cef.chunk.css"
+  },
+  {
+    "revision": "36274318ecd901e4c31aa9ee9b04c91f",
+    "url": "/Geodian/index.html"
+  }
+];
